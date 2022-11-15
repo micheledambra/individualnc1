aaabb5e429e7eed4e7db184a97fa2ed366905409
micheledambra <micheledambra@users.noreply.github.com>
2022-11-15 18:41:35 +0100

    Project initial commit

---
+++ individualnc1/individualnc1/individualnc1App.swift
@@ -0,0 +1,17 @@
+//
+//  individualnc1App.swift
+//  individualnc1
+//
+//  Created by Michele D'Ambra on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct individualnc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
